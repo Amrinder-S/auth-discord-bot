82ed7852146ecc588c0216a80c0c8b065f3c7dd6
AMR <amrSAMP@proton.me>
2023-08-29 19:26:01 +0530

    fixed regular expressions (probably)

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ ROLE_FIRST_YEAR = 1123938097957191810
 ROLE_SECOND_YEAR = 1123938146141347860
 ROLE_THIRD_YEAR = 1124275055749251092
 ROLE_FOURTH_YEAR = 1124275287648112700
-mail_pattern = r'^(?!.*abc_abc123@gndec\.ac\.in)(?:[a-zA-Z]+(?:_[0-9]{3}|[0-9]{3})@gndec\.ac\.in)$'
+mail_pattern = r'^[a-z]+\d+@gndec.ac.in$|^[a-z]+_23\d+@gndec.ac.in$'
 
 intents = discord.Intents.default()
 intents.members = True
